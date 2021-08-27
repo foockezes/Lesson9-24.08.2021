2577a466a16b32f3b1be1f4cd26d6fb1069ef50f
Musodzhon Zakirov <i.zulayho@gmail.com>
2021-08-27 17:25:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a2 = "NuN";
+            Console.WriteLine("Введите валюту usd, euro, rub, som");
+            string a1 = Console.ReadLine();
+            if (a1 == "som")
+            {
+                Console.WriteLine("введите вторую волюту usd, euro, rub");
+                a2 = Console.ReadLine();
+            }
+            Console.WriteLine("введите сумму конвертации");
+            double s = Convert.ToDouble(Console.ReadLine());
+                Converter conv = new Converter()
+            {
+                m1 = a1,
+                sum = s,
+                m2 = a2
+            };
+            conv.GetConv();
+        }
+    }
+    class Converter
+    {
+        public string m1;
+        public string m2;
+        public double conver;
+        public double sum;
+        public Converter() { }
+        public Converter(string M1, string M2, double Sum)
+        {
+           m1 = M1; m2 = M2; conver = Sum;
+        }
+        public void GetConv()
+        {
+            if (m1 == "usd")
+            {
+                conver = sum * 1143.86 / 100;
+                Console.WriteLine($"{sum} дол равняется {Math.Round(conver, 2)} сом");
+            }
+            else if (m1 == "som" && m2 == "usd")
+            {
+                conver = sum * 8.74 / 100;
+                Console.WriteLine($"{sum} сом равняется {Math.Round(conver, 2)} дол");
+            }
+            else if (m1 == "euro")
+            {
+                conver = sum * 1345.52 / 100;
+                Console.WriteLine($"{sum} эвро равняется {Math.Round(conver, 2)} сом");
+            }
+            else if (m1 == "som" && m2 == "euro")
+            {
+                conver = sum * 7.44 / 100;
+                Console.WriteLine($"{sum} сом равняется {Math.Round(conver, 2)} эвро");
+            }
+            else if (m1 == "rub")
+            {
+                conver = sum * 15.45 / 100;
+                Console.WriteLine($"{sum} руб равняется {Math.Round(conver, 2)} сом");
+            }
+            else if (m1 == "som" && m2 == "rub")
+            {
+                conver = sum * 647.60 / 100;
+                Console.WriteLine($"{sum} сом равняется {Math.Round(conver, 2)} эвро");
+            }
+            else Console.WriteLine("Введен не правильное значение");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Kolya = new Employee()
+            {
+                Name = "Kolya",
+                LastName = "Burov",
+                profession = "junior"
+            };
+            Kolya.GetInfo();
+            Employee andrey = new Employee()
+            {
+                Name = "andrey",
+                LastName = "Shefchenko",
+                profession = "senior"
+            };
+            andrey.GetInfo();
+            Employee fotima = new Employee()
+            {
+                Name = "Fotima",
+                LastName = "Alam",
+                profession = "midl"
+            };
+            fotima.GetInfo();
+        }
+    }
+    class Employee
+    {
+        public string Name;
+        public string LastName;
+        public string profession;
+        public double Salary;
+        public double tax;
+        public Employee() { }
+        public Employee(string prof, double salary, string name, string lastName, double Csalary) 
+        {
+            profession = prof; Salary = salary; Name = name; LastName = lastName; tax = Csalary;
+        }
+        public void GetInfo()
+        {
+            if (profession == "junior") Salary = 1000;
+            else if (profession == "midl") Salary = 2170;
+            else if (profession == "senior") Salary = 4500;
+            else Console.WriteLine("такой должности в системе нет");
+            tax = (Salary * 14) / 100;
+            Console.WriteLine($"Имя: {Name}, Фамилия: {LastName}, Должность: {profession}, Зарплата: {Salary}, налог: {tax}, чистая ЗП {Salary - tax}");
+        }
+        // я добавил 3 ий string чтобы вывести Должность "Надеюсь это не будет считаться минусом"
+    }
+}
